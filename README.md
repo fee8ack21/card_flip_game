6bc96025cd8e44b235ada716452add28cfdee497
fee8ack21 <73264834+fee8ack21@users.noreply.github.com>
2021-01-06 21:46:47 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# Card Flip Game
+---
+* https://fee8ack21.github.io/card_flip_game/
---
+++ js/all.js
@@ -0,0 +1,66 @@
+$(document).ready(function () {
+
+    // 執行卡牌隨機排列函式
+    random();
+
+    // 初始畫面
+    $('.card').addClass('rotate');
+
+    // 倒數計時
+    setTimeout(() => {
+      $('.card').removeClass('rotate');
+      // 執行點擊判斷函式
+      loop();
+    }, 1000);
+
+    // 卡牌隨機排列
+    function random() {
+      for (let i = 0; i <= $('.card').length; i++) {
+        var orderNumber = Math.floor(Math.random() * 10) + 1;
+        $('.card:nth-child(' + i + ')').css('order', orderNumber)
+      }
+    }
+
+    // 點擊判斷
+    function loop() {
+      var i = 0;
+      var img = [];
+      var imgInfo = [];
+      var imgLi = [];
+      // 
+      $('.card').click(function () {
+        $(this).addClass('rotate');
+        // 
+        img[i] = $(this);
+        imgInfo[i] = $(this).find('.front').attr('src');
+        imgLi[i] = $(this);
+        i++;
+        // 
+        if ($('.rotate').length % 2 == 0) {
+          // console.log(img);
+          // console.log(imgInfo);
+          // console.log(imgLi);
+          if (imgInfo[0] == imgInfo[1]) {
+            if ($('.rotate').length == $('.card').length) {
+              setTimeout(() => {
+                location.reload();
+              }, 1000);
+            } else {
+              $('.card').off('click');
+              setTimeout(() => {
+                loop()
+              }, 500);
+            }
+          } else {
+            $('.card').off('click');
+            setTimeout(() => {
+              imgLi[0].removeClass('rotate');
+              imgLi[1].removeClass('rotate');
+              loop()
+            }, 500);
+          }
+        }
+      })
+      $('.card.rotate').off('click')
+    }
+  })
